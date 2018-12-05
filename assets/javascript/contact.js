20d0c9a3a7aad391d8d565b7f68134153f5ac03a
sully34 <sssulai@gmail.com>
2018-12-04 20:49:55 -0500

    added sendmail integration code

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -22,6 +22,19 @@ function getInputVal(id){
     return document.getElementById(id).value;
 
 }
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
 
 <script src="https://www.gstatic.com/firebasejs/5.4.2/firebase.js"></script>
   // Initialize Firebase
