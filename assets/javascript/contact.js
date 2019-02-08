1268a488b77f5787af408a639d7f09161c94b13f
sully34 <sssulai@gmail.com>
2019-02-08 14:52:45 -0500

    updated css and index layout

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -1,58 +1,16 @@
-$(document).ready(function() {
-    if ($('#submit').length > 0 ) {
-        contactScript('forcontact');
-    }
-});
-document.getElementById(contactForm).addEventListener('submit', submitForm);
-
-function submitForm(e){
-    e.preventDefault();
-
-
-//get values
-var name = getInputValue('name');
-var email = getInputValue('email');
-var note = getInputValue('note');
-
-console.log(name);
+function handleSend(){
+    console.log("buttonPressed")
 }
 
-
-function getInputVal(id){
-    return document.getElementById(id).value;
-
+window.onload = function(){
+    console.log("initialized Contact Script")
+    document.getElementById("sendButton").addEventListener('click', function(e){
+        e.preventDefault()
+        console.log('clicked!')
+        var name = document.getElementById('name').value
+        var email = document.getElementById('email').value
+        var note = document.getElementById('note').value
+        var subject = name + " sent you a message through your website."
+        document.location = "mailto:sssulai@gmail.com" + "?subject="+subject+"&body="+note
+    })
 }
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
-
-
-<script src="https://www.gstatic.com/firebasejs/5.4.2/firebase.js"></script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCqoLntgM0rvHPZkkFbytPuh1Qf8krR9wQ",
-    authDomain: "contact-page-1c2fb.firebaseapp.com",
-    databaseURL: "https://contact-page-1c2fb.firebaseio.com",
-    projectId: "contact-page-1c2fb",
-    storageBucket: "contact-page-1c2fb.appspot.com",
-    messagingSenderId: "42781243222"
-  };
-  firebase.initializeApp(config);
-  var b = firebase.database().ref("messages");
-        $("#submit").submit(function(a) { $(this), console.log("Submit to Firebase");
-        var c = $("#name").val(),
-            d = $("#email").val(),
-            f = { name: c, email: d};
-        return b.push(f).then(function(a) { 
-            $(".sucess").css("display", "block"), 
-            $(".sucess-none").css("display", "none") }), !1
-         })
