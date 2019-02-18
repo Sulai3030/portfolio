c04848b624e85413a9c8ac1a11df4c839b43fc9b
sully34 <sssulai@gmail.com>
2019-02-18 15:03:17 -0500

    removed API keys

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,11 +1,12 @@
-echo "export SENDGRID_API_KEY='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'" > sendgrid.env;
-echo "sendgrid.env" >> .gitignore;
-source ./sendgrid.env
+<?php
+echo "'" > sendgrid.env;
+echo 'sendgrid.env >> .gitignore';
+source .sendgrid.env;
 
 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.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);
-const msg = {
+define (msg = {
   to: 'sully8899@gmail.com',
   from: 'sulai3030@gmail.com',
   subject: 'Sending with SendGrid is Fun',
@@ -13,3 +14,30 @@ const msg = {
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
 sgMail.send(msg);
+
+
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("test@example.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
