674d13e5106ae340ad6ee17c52072471757ab460
sully34 <sssulai@gmail.com>
2019-02-24 19:54:21 -0500

    fixed react compoents added react router and links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Container, Row, Col } from 'reactstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+import { Jumbotron, Button } from 'reactstrap';
+import { Link } from 'react-router';
 
-import Header from './components/Header'
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
 
-
-class App extends Component {
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <Header />
-      </div>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/"></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="https://www.linkedin.com/in/sulai3030/">LinkedIn</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/Sulai3030">GitHub</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://www.linkedin.com/in/sulai3030/">Projects</NavLink>
+              </NavItem>
+            </Nav>
+            <NavItem>
+                <NavLink href={`mailto:${this.props.email}`}>Contact</NavLink>
+              </NavItem>
+          </Collapse>
+        </Navbar>
+        <Jumbotron fluid>
+          <Container fluid>
+            <h1 className="display-3">Sulai Sivadel</h1>
+            <p className="lead">Ambitious, user-focused Full Stack Web Developer recently graduated from Columbia University Coding Boot Camp with experience working in teams. Possesses project management experience and experience delegating responsibilities to a team from time at Maximus where he spearheaded a project to create the audio/ADA compliant portion of a new healthcare plan for the disabled distributed in the state of New York. Sulai is flexible and a quick learner. Sulai is adaptable, communicative, knowledgeable and steadfast with excellent computer engineering skills learned in the Columbia University Coding Bootcamp where he created projects in HTML5, CSS, JavaScript, and React. Samples can be found here: <Link to="https://sulai3030.github.io/l"></Link></p>
+          </Container>
+      </Jumbotron>
+    </div>
     );
   }
 }
-
-export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-
-
-import{
-  BrowserRouter as Router,
-  Route,
-  Link,
-}from 'react-router-dom'
-
-import About from './components/About'
-import Contact from './components/Contact'
-import Header from './components/Header'
-import Projects from './components/Projects'
-
-ReactDOM.render(
-    <Router>
-      <div>
-        <Route path="/" component={App} />
-        <Route exact path="/" component={About} />
-        <Route path= "/projects" components={Projects} />
-       <Route path="/contact" component={Contact} />
-      </div>
-    </Router>
-    , 
-    document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
