fc6ef04ca4969e75f9394266b7a634750058b3b9
sully34 <sssulai@gmail.com>
2019-07-26 13:13:59 -0400

    Updates

---
+++ precache-manifest.cde5187ee018c7793b4a60de86f1a6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650fcc12c388e3a422285fde91734f6a",
+    "url": "/Sulai3030/portfolio3/index.html"
+  },
+  {
+    "revision": "b5bdc870179c900321c9",
+    "url": "/Sulai3030/portfolio3/static/css/main.995d1855.chunk.css"
+  },
+  {
+    "revision": "0345afdb88064cbbadd1",
+    "url": "/Sulai3030/portfolio3/static/js/2.dd537143.chunk.js"
+  },
+  {
+    "revision": "b5bdc870179c900321c9",
+    "url": "/Sulai3030/portfolio3/static/js/main.8f0de5fb.chunk.js"
+  },
+  {
+    "revision": "dd49fff14e603d445965",
+    "url": "/Sulai3030/portfolio3/static/js/runtime~main.a3d01953.js"
+  },
+  {
+    "revision": "814c5b813ba77e8926efe71d7e6b91ce",
+    "url": "/Sulai3030/portfolio3/static/media/seaPhoto.814c5b81.jpg"
+  }
+]);
